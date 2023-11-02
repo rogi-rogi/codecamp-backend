ce9e421636294925b3b8a831bf8914f4ac1c7c83
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2023-11-03 01:47:06 +0900

    Delete section01 directory

--- section01/01-08-welcome-template-api-import/utils.js
+++
@@ -1,8 +0,0 @@
-export function getToday() {
-  const cur = new Date();
-  const yyyy = cur.getFullYear();
-  const mm = cur.getMonth() + 1;
-  const dd = cur.getDate();
-
-  return `${yyyy}-${mm}-${dd}`;
-}
